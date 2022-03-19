dcdabf508a39b74d9c44a8e95fee0445fc8c07bb
Shriy1556k <81312355+Shriy1556k@users.noreply.github.com>
2022-03-19 12:48:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+noseX=0;
+noseY=0;
+
+function preload()
+{
+clown_n = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+}
+
+function setup()
+{
+ canvas = createCanvas(300,300);
+ canvas.center();
+ video = createCapture(VIDEO);
+ video.size(300,300);
+ video.hide();
+
+ pose = ml5.poseNet(video , modelLoaded);
+ pose.on( 'pose' , gotPoses);
+}
+
+function draw()
+{
+ image(video,0,0,300,300);
+ fill(255,0,0);
+ stroke(255,0,0);
+ circle(noseX,noseY,20);
+ image(clown_n,noseX,noseY,20,20);
+}
+
+function modelLoaded()
+{
+    console.log("pose net is ready");
+}
+
+function gotPoses(results)
+{
+  if (results.length > 0) {
+      console.log(results);
+      noseX=results[0].pose.nose.x-5;
+      noseY=results[0].pose.nose.y-5;
+      console.log(noseX);
+      console.log(noseY);
+  }
+}
+
+function takeSnapshot()
+{
+    save("clone_nose_animation.tiff");
+}
